1040966e9fba51df7e1b7ecf4f74ad720f7eaf99
Raul Marin <rmrodriguez@cartodb.com>
2019-12-26 15:38:20 +0100

    Dev: Update mocha to 6

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -75,9 +75,9 @@ echo "CREATE USER ${TESTUSER} WITH PASSWORD '${TESTPASS}';" | psql -v ON_ERROR_S
 
 if test x"$OPT_COVERAGE" = xyes; then
   echo "Running tests with coverage"
-  ./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha -- -u tdd -t 5000 $@ test/**/*.js
+  ./node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha -- -u bdd --exit -t 5000 $@ test/**/*.js
 else
   echo "Running tests"
-  ./node_modules/.bin/mocha -t 5000 -u tdd $@ test/**/*.js
+  ./node_modules/.bin/mocha -t 5000 -u bdd --exit $@ test/**/*.js
 fi
 exit $?
